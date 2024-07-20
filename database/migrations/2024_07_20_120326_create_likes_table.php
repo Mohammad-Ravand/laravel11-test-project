1dd77013cd31d7ee0092357c67421bae7c3811c7
Mohammad-Ravand <mohammadravandwebdeveloperiran@gmail.com>
2024-07-21 00:11:54 +0430

    inital commit anfter creating models and migrations

---
+++ database/migrations/2024_07_20_120326_create_likes_table.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign key field
+            $table->foreignId('comment_id')->constrained()->onDelete('cascade'); // Foreign key field
+            $table->enum('like',['like','dislike']);
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('likes');
+    }
+};
